ad4f9eb66ec672ef52808ab407e1c4b6f24553f1
Manjundhar <103714273+Manju025@users.noreply.github.com>
2025-11-04 08:59:01 +0530

    Adjust padding in Header component

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => (
     <div className="w-full h-auto bg-gray-700 rounded flex items-center justify-start">
         <img src = "/fastor-logo.svg" alt = "logo" className="w-25 ml-20 m-9"/>
-        <h1 className="text-xl font-bold w-full pr-25 font-mono text-sky-500">FASTOR APP</h1>
+        <h1 className="text-xl font-bold w-full pr-50 font-mono text-sky-500">FASTOR APP</h1>
     </div>
 )
 
